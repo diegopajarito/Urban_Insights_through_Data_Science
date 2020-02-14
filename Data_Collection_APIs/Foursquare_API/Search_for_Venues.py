221084363ffe45af8b58f75472e2a901b3e3157f
diegopajarito <diegopajarito@gmail.com>
2020-02-14 10:45:21 -0500

    Updates to optimise values for Foursquare API

--- Data_Collection_APIs/Foursquare_API/Search_for_Venues.py
+++ Data_Collection_APIs/Foursquare_API/Search_for_Venues.py
@@ -6,52 +6,48 @@ from geopy.geocoders import Nominatim
 import pandas as pd
 import requests
 
-# open csv file with pandas
-df_grid = pd.read_csv('# insert grid created in QGIS')
+# open csv file with containing the limits fo a grid cell array
+df_grid = pd.read_csv('../Path/to/grid.csv')
+# We
+df_grid = df_grid[['left', 'top']]
 
-# extract just the 'left' and 'top' columns
-df_grid_2 = df_grid[['left', 'top']]
+# Due to limitation of calls per day, you should use up to 500 elements for testing
+df_grid = df_grid.iloc[0:500]
 
-# extract some specific rows
-# (I need this line because I can make max 500 calls per day)
-df_grid_4 = df_grid_2.iloc[0:500]
-
-# create an empty list that will have all the dictionaries that I will retrieve inside
+# create an empty list to store the dictionaries containing data retrived in the loop
 list_of_dictionaries = []
 
 # url taken by the Foursquare API developers page -> Places API -> Venues -> Search for venues
 url = 'https://api.foursquare.com/v2/venues/search'
 # make a for loop to iterate through the points of the grid created in QGIS to extract all lot and lat
-for i in df_grid_4.index:
-  # this is to extract the lon and lat of each point
-  lon = df_grid_4['left'][i]
-  lat = df_grid_4['top'][i]
-  # convert lon and lat from float to srings so that I can concatenate them
-  lon = str(lon)
-  lat = str(lat)
-  # concatenate lon and lat
-  lat_lon = lat + ',' + lon
+for i in df_grid.index:
+    # this is to extract the lon and lat of each point
+    lon = df_grid['left'][i]
+    lat = df_grid['top'][i]
+    # concatenate lon and lat values. Be aware of the need to cast from number to string.
+    lat_lon = str(lat) + ',' + str(lon)
 
-# parameters taken from the API developers page 'search for venues' (same as above)
-  params = dict(
-    client_id='insert client_id here',
-    client_secret='insert client_secret here',
-    v='20180323',
-    ll=lat_lon,  # each time it is replaced with new coordinates
-    intent='browse',
-    radius=30,
-    limit=100
-  )
+    # parameters taken from the API developers page 'search for venues' (same as above)
+    params = dict(
+        client_id='insert client_id here',
+        client_secret='insert client_secret here',
+        v='20180323',
+        ll=lat_lon,  # each time it is replaced with new coordinates
+        intent='browse',
+        radius=30,
+        limit=100
+    )
 
-  # use the request.get method to ask for the parameters
-  resp = requests.get(url=url, params=params)
-  print(resp)
+    # use the request.get method to ask for the parameters
+    resp = requests.get(url=url, params=params)
+    # Print the raw response
+    print(resp)
 
-  # json.loads method transforms the response into a dictionary
-  data = json.loads(resp.text)
+    # json.loads method transforms the response into a dictionary
+    data = json.loads(resp.text)
 
-  # append to the new dictionary created
-  list_of_dictionaries.append(data)
+    # append to the new dictionary created
+    list_of_dictionaries.append(data)
 
 # set new lists to set a dataframe later on
 venues_id = []
@@ -62,22 +58,22 @@ category = []
 
 # this for loop iterates through the list of dictionaries to extract them one by one
 for det_venues in list_of_dictionaries:
-  # this second loop retrieves the parameters inside the json format
-  for i in det_venues['response']['venues']:
+    # this second loop retrieves the parameters inside the json format
+    for i in det_venues['response']['venues']:
 
-    # add all the ids to the venues_id list
-    venues_id.append(i['id'])
-    # add all the names to the names list
-    names.append(i['name'])
-    # add all the latitudes to the latitudes list
-    latitudes.append(i['location']['lat'])
-    # add all the latitudes to the latitudes list
-    longitudes.append(i['location']['lng'])
+        # add all the ids to the venues_id list
+        venues_id.append(i['id'])
+        # add all the names to the names list
+        names.append(i['name'])
+        # add all the latitudes to the latitudes list
+        latitudes.append(i['location']['lat'])
+        # add all the latitudes to the latitudes list
+        longitudes.append(i['location']['lng'])
 
-    # this for loop retrieve all the categories
-    for cat in i['categories']:
-      # add all the categories to the category list
-      category.append(cat['name'])
+        # this for loop retrieve all the categories
+        for cat in i['categories']:
+            # add all the categories to the category list
+            category.append(cat['name'])
 
 print('--------------------------- Underneath printed all the lists to check them ---------------------------------')
 print('venues id:', venues_id)
@@ -87,7 +83,8 @@ print('longitudes:', longitudes)
 print('category:', category)
 
 # create dataframe from lists names, latitudes, longitudes
-df_venues = pd.DataFrame(list(zip(venues_id, names, latitudes, longitudes, category)), columns=['Venues_id', 'Name', 'lat', 'lon', 'category'])
+df_venues = pd.DataFrame(list(zip(venues_id, names, latitudes, longitudes, category)),
+                         columns=['Venues_id', 'Name', 'lat', 'lon', 'category'])
 
 # drop rows with same values in column Venues_id
 df_venues_search = df_venues.drop_duplicates(subset=['Venues_id'], keep='last')
